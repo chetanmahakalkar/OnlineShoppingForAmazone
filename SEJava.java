980dbd6bb931268e43f12c46e3a46df88b6eaae7
chetanmahakalkar <46559465+chetanmahakalkar@users.noreply.github.com>
2020-10-21 23:24:35 +0530

    Add files via upload

---
+++ SEJava.java
@@ -0,0 +1,34 @@
+
+public class SEJava {
+
+		private int productId;
+		private String productName;
+		private String desc;
+		private double price;
+		public int getProductId() {
+			return productId;
+		}
+		public void setProductId(int productId) {
+			this.productId = productId;
+		}
+		public String getProductName() {
+			return productName;
+		}
+		public void setProductName(String productName) {
+			this.productName = productName;
+		}
+		public String getDesc() {
+			return desc;
+		}
+		public void setDesc(String desc) {
+			this.desc = desc;
+		}
+		public double getPrice() {
+			return price;
+		}
+		public void setPrice(double price) {
+			this.price = price;
+		}
+		
+		
+}
